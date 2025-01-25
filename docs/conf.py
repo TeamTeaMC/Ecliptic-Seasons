100b6c6eba9946e547f70c4d81be70589958478e
joe-vettek <my1233215@126.com>
2025-01-25 21:45:24 +0800

    Create conf.py

---
+++ docs/conf.py
@@ -0,0 +1,22 @@
+import os
+import sys
+
+sys.path.insert(0, os.path.abspath('..'))
+
+project = 'Ecliptic Seasons'
+author = 'jianzoushihu'
+version = '1.0'
+release = '1.0.0'
+
+extensions = [
+    'sphinx.ext.autodoc',
+    'sphinx.ext.viewcode',
+    'sphinx.ext.napoleon',
+]
+
+language = 'en'
+html_theme = 'alabaster'
+
+html_static_path = ['docs']
+
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
