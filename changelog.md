32d3cd07928fe82f85d071d15c9a2e38b72c61dc
joe-vettek <my1233215@126.com>
2025-04-18 21:26:24 +0800

    0.10.6.0.1

--- changelog.md
+++ changelog.md
@@ -1,3 +1,5 @@
+#### **0.10.6.0.1**
+
 ## **0.10.6**
 - Added `eclipticseasons:natural_plants` tag to identify Blocks that require growth control via compatibility mode — typically used for mods that do not natively support the Forge Event Bus or are added via Sinytra Connector (Fabric mods).
 - Added a config entry named as ForceCompatMode: forces all crops to use compatibility mode for growth control, not just those tagged with `eclipticseasons:natural_plants`. Note native support offers significantly better performance.
--- src/main/java/com/teamtea/eclipticseasons/api/event/CanPlantGrowEvent.java
+++ src/main/java/com/teamtea/eclipticseasons/api/event/CanPlantGrowEvent.java
@@ -4,12 +4,14 @@ import net.minecraft.core.BlockPos;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraftforge.event.level.BlockEvent;
+import net.minecraftforge.eventbus.api.Event;
 
 
 /**
  * Fired when a crop block seems to grow and need extra check since it can not fire forge event.
  *
  */
+@Event.HasResult
 public class CanPlantGrowEvent extends BlockEvent.CropGrowEvent implements IESEvent{
     public CanPlantGrowEvent(Level level, BlockPos pos, BlockState state) {
         super(level, pos, state);
