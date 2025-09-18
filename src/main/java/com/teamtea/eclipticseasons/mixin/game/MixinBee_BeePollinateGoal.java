dca5c50f4f715d658da4d9ee5b64670aa58cac0b
joe-vettek <my1233215@126.com>
2025-09-18 14:08:44 +0800

    0.12.0-pre19-1-1

--- src/main/java/com/teamtea/eclipticseasons/mixin/game/MixinBee_BeePollinateGoal.java
+++ src/main/java/com/teamtea/eclipticseasons/mixin/game/MixinBee_BeePollinateGoal.java
@@ -1,31 +1,38 @@
 package com.teamtea.eclipticseasons.mixin.game;
 
 
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
 import com.teamtea.eclipticseasons.common.game.AnimalHooks;
+import net.minecraft.core.BlockPos;
 import net.minecraft.world.entity.animal.Bee;
-import org.spongepowered.asm.mixin.Dynamic;
-import org.spongepowered.asm.mixin.Final;
-import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.*;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+import java.util.Optional;
 
 @SuppressWarnings("target")
 @Mixin(value = {Bee.BeePollinateGoal.class})
 public class MixinBee_BeePollinateGoal {
 
-    @Shadow@Dynamic
+    @Shadow
+    @Dynamic
     @Final
     Bee this$0;
 
+    @Unique
+    private int eclipticseasons$season_checkTime = 0;
 
-    @Inject(at = {@At("RETURN")}, method = {"canBeeUse"}, cancellable = true)
-    public void eclipticseasons$canBeeUse(CallbackInfoReturnable<Boolean> cir) {
-        if (cir.getReturnValue()) {
-            if (AnimalHooks.cancelBeePollinate(this$0)) {
-                cir.setReturnValue(false);
-            }
+
+    @WrapOperation(at = {@At(value = "INVOKE", target = "Lnet/minecraft/world/entity/animal/Bee$BeePollinateGoal;findNearbyFlower()Ljava/util/Optional;")}, method = {"canBeeUse"})
+    public Optional<BlockPos> eclipticseasons$canBeeUse(Bee.BeePollinateGoal instance, Operation<Optional<BlockPos>> original) {
+        if (eclipticseasons$season_checkTime >= 0) {
+            eclipticseasons$season_checkTime--;
+            return Optional.empty();
+        } else if (AnimalHooks.cancelBeePollinate(this$0)) {
+            eclipticseasons$season_checkTime = 40;
+            return Optional.empty();
         }
+        return original.call(instance);
     }
 }
