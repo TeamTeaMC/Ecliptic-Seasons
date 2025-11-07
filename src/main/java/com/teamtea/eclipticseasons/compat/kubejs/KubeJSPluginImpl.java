dfed293dddededea6b02f1a2b798a66887d0468d
joe-vettek <my1233215@126.com>
2025-11-07 11:55:44 +0800

    js

---
+++ src/main/java/com/teamtea/eclipticseasons/compat/kubejs/KubeJSPluginImpl.java
@@ -0,0 +1,44 @@
+package com.teamtea.eclipticseasons.compat.kubejs;
+
+import com.teamtea.eclipticseasons.EclipticSeasons;
+import com.teamtea.eclipticseasons.api.EclipticSeasonsApi;
+import com.teamtea.eclipticseasons.api.util.EclipticUtil;
+import com.teamtea.eclipticseasons.client.util.ClientCon;
+import com.teamtea.eclipticseasons.common.core.snow.SnowyMapChecker;
+import dev.latvian.mods.kubejs.KubeJSPlugin;
+import dev.latvian.mods.kubejs.script.BindingsEvent;
+import net.minecraft.core.BlockPos;
+import net.minecraft.core.SectionPos;
+import net.minecraft.server.level.ServerLevel;
+import net.minecraft.world.level.Level;
+
+public class KubeJSPluginImpl extends KubeJSPlugin {
+
+    public static final ESBindings ES_BINDINGS = new ESBindings();
+
+    @Override
+    public void registerBindings(BindingsEvent event) {
+        event.add("EclipticSeasonsApi", EclipticSeasonsApi.getInstance());
+        event.add("EclipticSeasonsBindings", ES_BINDINGS);
+    }
+
+    public static class ESBindings {
+        public boolean testInput(Object obj) {
+            EclipticSeasons.logger(obj.toString(), "9431043");
+            return true;
+        }
+
+        public boolean canSnowyBlockInteract() {
+            return EclipticUtil.canSnowyBlockInteract();
+        }
+
+        public boolean removeSnowyStatus(Level level, BlockPos blockpos) {
+            if (level instanceof ServerLevel serverLevel) {
+                SnowyMapChecker.removeSnowyStatus(serverLevel, blockpos);
+            } else {
+                ClientCon.agent.setChunkDirty(SectionPos.of(blockpos));
+            }
+            return true;
+        }
+    }
+}
