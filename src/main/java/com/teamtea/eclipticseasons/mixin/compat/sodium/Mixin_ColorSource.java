67e4484925de2dc507471c1dafc801013a90b398
joe-vettek <my1233215@126.com>
2026-07-28 14:02:03 +0800

    0.14.2.1

--- src/main/java/com/teamtea/eclipticseasons/mixin/compat/sodium/Mixin_ColorSource.java
+++ src/main/java/com/teamtea/eclipticseasons/mixin/compat/sodium/Mixin_ColorSource.java
@@ -28,8 +28,9 @@ public abstract class Mixin_ColorSource implements SodiumBlenderColorProvider {
             cancellable = true)
     private void eclipticseasons$getColors_forESLeaves(
             LevelSlice slice, BlockPos pos, BlockPos.MutableBlockPos scratchPos, BlockState state, ModelQuadView quad, int[] output, boolean smooth, CallbackInfo ci) {
-        if (this.color instanceof SodiumBlenderColorProvider sodiumBlenderColorProvider)
+        if (this.color instanceof SodiumBlenderColorProvider sodiumBlenderColorProvider) {
             sodiumBlenderColorProvider.getColors(slice, pos, scratchPos, state, quad, output, smooth);
-        ci.cancel();
+            ci.cancel();
+        }
     }
 }
