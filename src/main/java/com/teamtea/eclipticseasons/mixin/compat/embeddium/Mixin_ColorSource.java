edee37f1e090efb1312d3a0bfb41f0c863abfbdc
joe-vettek <my1233215@126.com>
2026-08-23 23:19:17 +0800

    fix

--- src/main/java/com/teamtea/eclipticseasons/mixin/compat/embeddium/Mixin_ColorSource.java
+++ src/main/java/com/teamtea/eclipticseasons/mixin/compat/embeddium/Mixin_ColorSource.java
@@ -1,5 +1,7 @@
 package com.teamtea.eclipticseasons.mixin.compat.embeddium;
 
+import com.teamtea.eclipticseasons.common.mixin.condition.ConditionalMixin;
+import com.teamtea.eclipticseasons.common.mixin.condition.ModCondition;
 import com.teamtea.eclipticseasons.common.mixin.injector.DirectInject;
 import com.teamtea.eclipticseasons.compat.embeddium.EmbeddiumBlenderColorProvider;
 import me.jellysquid.mods.sodium.client.model.quad.ModelQuadView;
@@ -12,6 +14,7 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 
+@ConditionalMixin(noneOf = {@ModCondition(value = "embeddium", name = "Celeritas")})
 @Mixin(targets = "me.jellysquid.mods.sodium.client.model.color.DefaultColorProviders$VanillaAdapter")
 public abstract class Mixin_ColorSource implements EmbeddiumBlenderColorProvider {
 
--- src/main/java/com/teamtea/eclipticseasons/mixin/compat/embeddium/Mixin_FoliageColorSource.java
+++ src/main/java/com/teamtea/eclipticseasons/mixin/compat/embeddium/Mixin_FoliageColorSource.java
@@ -1,9 +1,12 @@
 package com.teamtea.eclipticseasons.mixin.compat.embeddium;
 
 import com.teamtea.eclipticseasons.client.color.season.FoliageColorSource;
+import com.teamtea.eclipticseasons.common.mixin.condition.ConditionalMixin;
+import com.teamtea.eclipticseasons.common.mixin.condition.ModCondition;
 import com.teamtea.eclipticseasons.compat.embeddium.EmbeddiumBlenderColorProvider;
 import org.spongepowered.asm.mixin.Mixin;
 
 @Mixin({FoliageColorSource.class, FoliageColorSource.Impl.class})
+@ConditionalMixin(noneOf = {@ModCondition(value = "embeddium", name = "Celeritas")})
 public abstract class Mixin_FoliageColorSource implements EmbeddiumBlenderColorProvider {
 }
